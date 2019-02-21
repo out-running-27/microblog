8c795b5c0e4c834e0e229fb3343ff17016d7811a
Paul Russum <paul.russum@gmail.com>
2019-02-21 08:06:21 -0600

    Completed Chapter 12 - Dates and Times and added requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,6 +18,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
